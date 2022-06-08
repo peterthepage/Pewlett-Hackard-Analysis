33e524f14aba22aa5537680bf8f7d44eb3ebc3b1
peterthepage <mastermind723.pp@gmail.com>
2022-06-08 16:03:58 -0500

    Deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -14,9 +14,20 @@ FROM retirement_titles AS rt
 WHERE (to_date = '9999-01-01')
 ORDER BY emp_no, to_date  DESC;
 
--- Create table with count of retirees
+-- Create table with count of retirees by title
 SELECT COUNT(ut.emp_no), ut.title
 INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY title
 ORDER BY COUNT(ut.emp_no) DESC;
+
+-- Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name,
+e.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_eligibility
+FROM employees AS e INNER JOIN
+titles as ti ON (e.emp_no = ti.emp_no)
+INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
